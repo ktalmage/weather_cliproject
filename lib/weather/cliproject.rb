ed84ee560ce84bbf53fd5acecd6251f8c999730e
Kyle Talmage <ktalmage85@gmail.com>
2020-03-02 19:49:44 +0000

    first commit

---
+++ lib/weather/cliproject.rb
@@ -0,0 +1,8 @@
+require "weather/cliproject/version"
+
+module Weather
+  module Cliproject
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/weather/cliproject/version.rb
@@ -0,0 +1,5 @@
+module Weather
+  module Cliproject
+    VERSION = "0.1.0"
+  end
+end
